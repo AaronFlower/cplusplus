41a844c6161d63917caca418b959dd88de5ff573
AaronFlower <haojunzhan@gmail.com>
2018-02-02 16:22:30 +0800

    add configurable settings for cmake

---
+++ makefile-tutorial/06-cmake-demo04/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+
+#ifdef USE_MYMATH
+	#include "math/myPower.h"
+#else
+	#include <math.h>
+#endif
+
+
+int main (int argc, char *argv[])
+{
+	if (argc < 3) {
+		printf("Please input base and exponent to rasise base ^ exponent!\n");
+		return 0;
+	}
+
+	int base = atoi(argv[1]);
+	int exponent = atoi(argv[2]);
+
+	#ifdef USE_MYMATH
+		printf("Now we use our own Math library. \n");
+		long int result = power(base, exponent);
+	#else
+		printf("Now we use standard library. \n");
+		long int result = pow(base, exponent);
+	#endif
+	
+	printf(" %d ^ %d = %ld \n", base, exponent, result);
+	return 0;
+}
