743e292b7bd3e82c09e8eae1a60d431adefc7ca6
AaronFlower <haojunzhan@gmail.com>
2019-04-28 11:06:36 +0800

    add BM first heuristic method -- bad character

--- algo/01-string-search/solution.h
+++ algo/01-string-search/solution.h
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <assert.h>
 
+#define CHAR_COUNT 256
+
 int naiveStrSearch(const char* hayStack, const char* needle, size_t start) {
     assert(hayStack != NULL);
     assert(needle != NULL);
@@ -26,6 +28,47 @@ int naiveStrSearch(const char* hayStack, const char* needle, size_t start) {
         }
 
         if (j >= nLen) return i;
+    }
+    return -1;
+}
+
+void initBadCharMap(int *badCharMap, const char *needle, size_t len) {
+    for (size_t i = 0; i < CHAR_COUNT; ++i) {
+        badCharMap[i] = -1;
+    }
+
+    for (size_t i = 0; i < len; ++i) {
+        badCharMap[(int)needle[i]] = i;
+    }
+}
+
+/**
+ * Boyer-Moore string search algorithm using the first heuristic method:
+ * bad character heuristic.
+ */
+int BMStrSearchBadChar(const char *hayStack, const char *needle, size_t start) {
+    assert(hayStack != NULL);
+    assert(needle != NULL);
+
+    size_t hLen = strlen(hayStack);
+    size_t nLen = strlen(needle);
+    assert(start < hLen);
+
+    if (nLen == 0) return start;
+
+    int badCharMap[CHAR_COUNT];
+    initBadCharMap(badCharMap, needle, nLen);
+
+    size_t i = start + nLen - 1;
+    size_t j = 0;
+    while (i < hLen) {
+        for (j = 0; j < nLen; ++j) {
+            if (hayStack[i - j] != needle[nLen - j - 1]) {
+                break;
+            }
+        }
+        if (j >= nLen) return i - nLen + 1;
+        i = i + (nLen - 1 - badCharMap[(int)hayStack[i]]);
     }
     return -1;
 }
--- algo/01-string-search/test.c
+++ algo/01-string-search/test.c
@@ -37,8 +37,42 @@ static void test_naive_string_search() {
 
 }
 
+static void test_BM_bad_char_heuristic() {
+    /* char *h1 = NULL; */
+    /* char *n1 = NULL; */
+    /* EXPECT_EQ_INT(0, naiveStrSearch(h1, n1, 0)); */
+
+    char *hayStack = "Hello";
+    char *needle = "";
+    EXPECT_EQ_INT(0, BMStrSearchBadChar(hayStack, needle, 0));
+    EXPECT_EQ_INT(1, BMStrSearchBadChar(hayStack, needle, 1));
+
+    char *n2 = "e";
+    EXPECT_EQ_INT(1, BMStrSearchBadChar(hayStack, n2, 1));
+
+    char *n3 = "r";
+    EXPECT_EQ_INT(-1, BMStrSearchBadChar(hayStack, n3, 1));
+
+    int pos[3] = {2, 3, -1};
+    int start = -1;
+    for (size_t i = 0; i < 3; ++i) {
+        start = BMStrSearchBadChar(hayStack, "l", start + 1);
+        EXPECT_EQ_INT(pos[i], start);
+    }
+
+    char *h4 = "this is a simple example";
+    char *n4 = "ple";
+    int pos4[3] = {13, 21, -1};
+    start = -1;
+    for (size_t i = 0; i < 3; ++i) {
+        start = BMStrSearchBadChar(h4, n4, start + 1);
+        EXPECT_EQ_INT(pos4[i], start);
+    }
+}
+
 int main()
 {
     test_naive_string_search();
+    test_BM_bad_char_heuristic();
     OUTPUT_TEST();
 }
