0362804fbdfa4653b2f57e79a432b80ce77543b7
AaronFlower <haojunzhan@gmail.com>
2019-07-02 23:38:51 +0800

    kdTree init

---
+++ 15-kdTree/point.h
@@ -0,0 +1,19 @@
+/**
+ * File: point.h
+ * A class representating a point in N-dimensional space
+ * Point is parameterized over an integer N.
+ * This allows the compiler to verify the type being used correctly
+ */
+#ifndef POINT_HEAD__
+#define POINT_HEAD__
+
+#include <cmath>
+#include <algorithm>
+
+template<std::size_t N>
+class Point {
+public:
+    // Types representating iterators that can traverse and optionally modify the elements of the Point.
+
+};
+#endif /* ifndef POINT_HEAD__ */
---
+++ 15-kdTree/test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "solution.h"
+
+#include <iostream>
+
+TEST(test, INIT) {
+    std::cout << "Hello 15-kdTree" << std::endl;
+}
