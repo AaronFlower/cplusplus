7895e2e716acad1c66176511e9bb9a4d7d328185
AaronFlower <haojunzhan@gmail.com>
2019-07-03 13:11:10 +0800

    add Point for kdtree

--- 15-kdTree/point.h
+++ 15-kdTree/point.h
@@ -7,13 +7,104 @@
 #ifndef POINT_HEAD__
 #define POINT_HEAD__
 
-#include <cmath>
-#include <algorithm>
+#include <cassert>    // for cassert
+#include <algorithm>  // for equal
 
-template<std::size_t N>
+using std::size_t;
+
+template<size_t N>
 class Point {
 public:
-    // Types representating iterators that can traverse and optionally modify the elements of the Point.
+    // Types representating iterators that can traverse and optionally modify
+    // the elements of the Point.
+    typedef double* iterator;
+    typedef const double* const_iterator;
 
+    size_t size();
+
+    // Queries or retrieves the value of the point at a particular point.
+    double& operator[](size_t index);
+    double operator[](size_t index) const;
+
+    // Returns iterators delineating the full range of elements in the Point.
+    iterator begin();
+    iterator end();
+    const_iterator begin() const;
+    const_iterator end() const;
+
+private:
+    double coords[N];
 };
+
+// Using non-member method to overload operator `==` and `!=`
+template <size_t N>
+bool operator==(const Point<N> &lhs, const Point<N> &rhs);
+
+template <size_t N>
+bool operator!=(const Point<N> &lhs, const Point<N> &rhs);
+
+// Return the Squared Euclidean distance between two points.
+template <size_t N>
+double EuclideanDistance(const Point<N> &lhs, const Point<N> &rhs);
+
+/** - - - - - Implimentation - - - - -  **/
+
+template <size_t N>
+size_t Point<N>::size() {
+    return N;
+}
+
+template <size_t N>
+double& Point<N>::operator[](size_t index) {
+    assert(index < N);
+    return &coords[index];
+}
+
+template <size_t N>
+double Point<N>::operator[](size_t index) const {
+    assert(index < N);
+    return coords[index];
+}
+
+
+template <size_t N>
+typename Point<N>::iterator Point<N>::begin() { // Don't miss typename
+    return coords;
+}
+
+template <size_t N>
+typename Point<N>::iterator Point<N>::end() {
+    return coords + N;
+}
+
+template <size_t N>
+typename Point<N>::const_iterator Point<N>::begin() const {
+    return coords;
+}
+
+template <size_t N>
+typename Point<N>::const_iterator Point<N>::end() const {
+    return coords + N;
+}
+
+template <size_t N>
+bool operator==(const Point<N> &lhs, const Point<N> &rhs) {
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+template <size_t N>
+bool operator!=(const Point<N> &lhs, const Point<N> &rhs) {
+    return !(lhs == rhs);
+}
+
+template <size_t N>
+double EuclideanDistance(const Point<N> &lhs, const Point<N> &rhs) {
+    double dist = 0.0;
+    for (size_t i = 0; i < N; ++i) {
+        double diff = lhs[i] - rhs[i];
+        dist += diff * diff;
+    }
+    return dist;
+}
+
 #endif /* ifndef POINT_HEAD__ */
--- 15-kdTree/test.cpp
+++ 15-kdTree/test.cpp
@@ -1,9 +1,22 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "solution.h"
+#include "point.h"
 
 #include <iostream>
+#include <vector>
 
-TEST(test, INIT) {
-    std::cout << "Hello 15-kdTree" << std::endl;
+using std::cout;
+using std::vector;
+
+TEST(test, Point) {
+    vector<double> nums1{1, 2, 3, 4};
+    vector<double> nums2{3, 4, 5, 6};
+    Point<4> p1, p2;
+    copy(nums1.begin(), nums1.end(), p1.begin());
+    copy(nums2.begin(), nums2.end(), p2.begin());
+
+    EXPECT_EQ(p1.size(), 4);
+    EXPECT_EQ(p2.size(), 4);
+    EXPECT_EQ(EuclideanDistance(p1, p2), 16);
 }
