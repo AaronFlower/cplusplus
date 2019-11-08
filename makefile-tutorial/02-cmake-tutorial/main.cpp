3149c828f4fe7886eb0b32afbb39325758e21db1
AaronFlower <haojunzhan@gmail.com>
2019-11-08 13:38:12 +0800

    cmake fix warnings

--- makefile-tutorial/02-cmake-tutorial/main.cpp
+++ makefile-tutorial/02-cmake-tutorial/main.cpp
@@ -19,8 +19,8 @@ int equalityTest(
 
 
 int main(
-        int argc,
-        char *argv[]
+        int,
+        char**
 )
 {
     int result = 0;
@@ -31,7 +31,7 @@ int main(
     list.addTask("compile");
     list.addTask("test");
 
-    result |= EXPECT_EQUAL(list.size(), 3);
+    result |= EXPECT_EQUAL(list.size(), size_t(3));
     result |= EXPECT_EQUAL(list.getTask(0), "write code");
     result |= EXPECT_EQUAL(list.getTask(1), "compile");
     result |= EXPECT_EQUAL(list.getTask(2), "test");
