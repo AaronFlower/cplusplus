72f5f3d4ccfd99760617c3c2e54faaadf6d14cc8
eason <zhanhaojun@bilibili.com>
2020-07-05 23:54:33 +0800

    1. update async test

--- cpp-demo/01-perfect-forward/perfect_forward_test.cpp
+++ cpp-demo/01-perfect-forward/perfect_forward_test.cpp
@@ -3,6 +3,16 @@
 //
 #include <gtest/gtest.h>
 #include "move_checker.h"
+#include <future>
+
+// print the content of an iterable
+template <typename Iterable>
+void printContents(const Iterable & iterable) {
+    for (auto e:iterable) {
+        std::cout << e << std::endl;
+    }
+}
+
 
 TEST(MOVE_CHECKER, begin) {
     move_checker checker;
@@ -17,5 +27,22 @@ TEST(MOVE_CHECKER, begin) {
     move_checker moved(std::move(checker));
     EXPECT_EQ(moved.copies(), 1);
     EXPECT_EQ(moved.moves(), 1);
+}
+
+TEST(MOVE_CHECKER, async) {
+    move_checker checker;
+
+    EXPECT_EQ(checker.copies(), 0);
+    EXPECT_EQ(checker.moves(), 0);
+
+    std::future<void> task = std::async(
+            std::launch::async,
+            printContents<move_checker>,
+                    checker
+            );
+
+    task.wait();
+    EXPECT_EQ(checker.copies(), 1);
+    EXPECT_EQ(checker.moves(), 2);
 }
 
